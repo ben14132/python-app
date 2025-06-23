1698e77e2709d7283a74ac991c71bfeb19f153aa
ben14132 <blee070@e.ntu.edu.sg>
2025-06-23 16:53:14 +0800

    uses self-hosted runners

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': 'you are doing greatttt :)'
+        'message': 'you are doing greatttt!!! :)'
     })
 
 @app.route('/api/v1/health')
