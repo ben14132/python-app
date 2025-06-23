85f78c8878c116fbe7c028657854d858a4921743
ben14132 <blee070@e.ntu.edu.sg>
2025-06-23 17:37:45 +0800

    added argocd installation

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': 'you are doing greatttt!!! woooo :)'
+        'message': 'you are doing greatttt!!! wooohooooo :)'
     })
 
 @app.route('/api/v1/health')
