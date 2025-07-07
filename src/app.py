515863def855f00e5e65e136ee75bb4a365da3a7
Bennett Lee <bennett.lee@olamnet.com>
2025-07-07 12:18:23 +0800

    helm chart deployment image tag changed 4

--- src/app.py
+++ src/app.py
@@ -10,7 +10,7 @@ def details():
     return jsonify({
         'time': datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
         'hostname': socket.gethostname(),
-        'message': 'you are doing greattttt!!! wooohhhhhhooo :)))'
+        'message': 'you are doing greattttt!!! wooohhhhooo :)))'
     })
 
 @app.route('/api/v1/health')
